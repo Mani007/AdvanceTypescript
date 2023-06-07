c9daee83c87ac8f0587be295637aa0b12563199e
Mohnish <mohnish.a007@gmail.com>
2023-06-07 19:07:02 +0100

    premitive types of typescript

--- basics/basic.js
+++ basics/basic.js
@@ -2,3 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var greeting = "hello";
 console.log(greeting);
+//number
+var userId = 3344;
+//boolean
+var admin = true;
--- basics/basic.ts
+++ basics/basic.ts
@@ -1,4 +1,9 @@
 let greeting:string = "hello";
 console.log(greeting)
 
+//number
+let userId: number = 3344
+
+//boolean
+let admin: boolean = true
 export {}
