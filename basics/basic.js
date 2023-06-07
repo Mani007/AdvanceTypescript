705d052611373d4625cbe14b69c8c462f37b5e60
Mohnish <mohnish.a007@gmail.com>
2023-06-07 19:12:08 +0100

    ANY  types of typescript

--- basics/basic.js
+++ basics/basic.js
@@ -6,3 +6,9 @@ console.log(greeting);
 var userId = 3344;
 //boolean
 var admin = true;
+//any type in typescript - you must avoid this type at all the cost as musch as possible as typechecking is completely stop here. 
+var hero; //this hero variable is automatically assigned as "any" type
+function getHero() {
+    return "thor";
+}
+hero = getHero();
--- basics/basic.ts
+++ basics/basic.ts
@@ -6,4 +6,12 @@ let userId: number = 3344
 
 //boolean
 let admin: boolean = true
+
+//any type in typescript - you must avoid this type at all the cost as musch as possible as typechecking is completely stop here. 
+let hero;  //this hero variable is automatically assigned as "any" type
+function getHero(){
+    return "thor"
+}
+
+hero = getHero()
 export {}
