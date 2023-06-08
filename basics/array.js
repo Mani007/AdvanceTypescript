ca7db9eb89987ed0282b9318766577b4d3ad4645
Mohnish <mohnish.a007@gmail.com>
2023-06-08 22:41:54 +0100

    Array in Ts

---
+++ basics/array.js
@@ -0,0 +1,7 @@
+// Defing the array
+var myarray = [];
+var myarraynum = []; // a slightly different syntax
+// adding item in array
+myarray.push("abc");
+console.log(myarray);
+var myusers = []; // array of user object
---
+++ basics/array.ts
@@ -0,0 +1,16 @@
+// Defing the array
+const myarray: string[] = []
+const myarraynum: Array<number> = []  // a slightly different syntax
+// adding item in array
+myarray.push("abc")
+
+console.log(myarray)
+
+type User = {
+    name: string,
+    dob: Date
+}
+
+const myusers: User[] = []   // array of user object
+
+
