4df2fce59b05288154977204c4ba845cac4e8112
Mohnish <mohnish.a007@gmail.com>
2023-06-11 00:46:47 +0100

    Class inheritance and protected  in TS

--- classes/classes.ts
+++ classes/classes.ts
@@ -1,6 +1,6 @@
 class User {
     public email: string
-    private _count: number = 1
+    protected _count: number = 1   // making it inheritale
     name: string
     private city: string = 'lisbon'   // private hence need getter and setter
     readonly #dob?: number             // # is used to mae private
@@ -18,12 +18,20 @@ class User {
         return this._count
     }
 
-    // setter
+    // setter  it does not have return type
     set setcount(num: number){
         this._count = num
     }
+
 
 }
 
 const jj = new User('a@a.com', 'jj')
 // jj.city = 'abc'
+// Inheritance in TS, but private variale cannot be used
+class Subuser extends User {
+    isFamily: boolean = true
+    changecount(num: number){
+        this._count = num   
+    }
+}
