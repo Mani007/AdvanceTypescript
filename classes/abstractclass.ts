376ceb961a59c10109027f597bc9d7c88848b613
Mohnish <mohnish.a007@gmail.com>
2023-06-11 01:01:37 +0100

    Abstract Classes  in TS

---
+++ classes/abstractclass.ts
@@ -0,0 +1,23 @@
+// astract class
+abstract class  TakePhoto {
+    constructor(
+        public camera: string,
+        public filter: string
+    ) {}
+    abstract getSep(): void
+}
+
+class Instagram extends TakePhoto{
+    constructor(
+        public camera: string,
+        public filter: string,
+        num: number
+    ) {
+        super(camera,filter)
+    }
+
+    getSep(): void {
+        console.log('abc')
+    }
+}
+const pic = new Instagram('abc', 'abc', 5)
