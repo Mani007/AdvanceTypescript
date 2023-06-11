31a220146dbc7e48d8fe8682b4b050d962892164
Mohnish <mohnish.a007@gmail.com>
2023-06-11 01:28:50 +0100

    Generics with interfaces in TS

--- Generics/generics.ts
+++ Generics/generics.ts
@@ -28,3 +28,19 @@ function getSearch<T>( prod: T[]):  T {
 const getmoresearch = <T,>(prod: T[]): T =>{    // the , comma is also OK in this line 'T,'
     return prod[3]
 }
+
+interface DB {
+    conn: string,
+    pass: string
+}
+// Multiple generics
+function another<T, U extends DB> ( val1: T, val2: U): object {
+    return{
+        val1, val2
+    }
+}
+
+another( 3, {
+    conn: 'conn',
+    pass: 'pass'
+})
